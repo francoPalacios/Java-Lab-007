91d68948c8bad45303e52c4a37e5caf6f922922e
francoPalacios <112013242+francoPalacios@users.noreply.github.com>
2022-10-12 13:28:43 -0700

    save

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -4,6 +4,15 @@ public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        int n = s.nextInt();
+        int t;
+        for(t = 0; t < n; t++ ){
+
+            System.out.printf("the sum is " + t);
+
+        }
+
+
     }
 
     public static void rangeSum() {
